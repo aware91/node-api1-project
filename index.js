512bf8fd1f15e0031ecb204b6119309ad216de2d
Anthony Ware <aware91coding@gmail.com>
2020-07-08 21:45:24 -0500

    have the base code and all the CRUD working. working on adding 500 error message and how to make sure you have name and bio before it post or put.

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const shortid = require('shortid');
+
+const server = express();
+
+let user = [];
+
+server.use(express.json());
+
+server.get('/', (req, res)=>{
+  res.json({User: 'User and Bio'});
+});
+
+const PORT = 5000;
+server.listen(PORT, ()=>{
+  console.log('listening on localhost:', PORT)
+});
+
+// Create
+server.post('/api/users', (req, res)=>{
+  const Created = req.body;
+  Created.id = shortid.generate();
+  user.push(Created);
+  res.status(201).json(Created);
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+        errorMessage: "User account could not be created",
+        message: err.message
+    });
+});
+
+// Read
+server.get('/api/users', (req, res)=>{
+  res.json(user);
+});
+
+server.get('/api/users/:id', (req, res)=>{
+  const {id} = req.params;
+  const userId = user.find(users => users.id === id);
+  if (userId) {
+    user = user.filter(users => users.id !== id);
+    res.status(200).json(userId)
+  } else {
+    res.status(404).json({message: 'The user with the specified ID does not exist.'})
+  }
+})
+
+// Delete
+server.delete('/api/users/:id', (req, res)=>{
+  const {id} = req.params;
+  const deleted = user.find(users => users.id === id);
+  if (deleted) {
+    user = user.filter(users => users.id !== id);
+    res.status(200).json(deleted)
+  } else {
+    res.status(404).json({message: 'The user with the specified ID does not exist.'})
+  }
+})
+
+// Update
+server.put('/api/users/:id', (req, res)=>{
+  const {id} = req.params;
+  const changes = req.body;
+  let index = user.findIndex(users => users.id === id);
+  if (index !== -1) {
+    // changes.id = id;
+    user[index] = changes;
+    res.status(200).json(user[index]);
+  } else {
+    res.status(404).json({ message: "The user with the specified ID does not exist."})
+  }
+})
